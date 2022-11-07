0a8600fd63f4fe30ca53e3c8bf60522a77f2bbb5
SergejZagorya <107615722+SergejZagorya@users.noreply.github.com>
2022-11-07 21:04:34 +0200

    aaa

--- js/task-02.js
+++ js/task-02.js
@@ -1,12 +1,9 @@
 const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
 
 const listEl = document.querySelector("#ingredients");
-console.log("listEl", listEl);
 
-console.log(ingredients);
 for (let i = 0; i < ingredients.length; i += 1) {
   const ingredient = ingredients[i];
-  // console.log(ingredient);
 
   const itemEl = document.createElement("li");
   itemEl.textContent = ingredient;
